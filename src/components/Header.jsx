67fb80a0924a0ba58a6d8321f3bb4da54dea9e36
Alex <alemumu207@gmail.com>
2024-08-26 13:05:25 +0200

    Fix: duplicidad etiqueta header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,41 +2,39 @@ import { useState } from "react";
 import styled from "styled-components";
 
 const StyledHeader = styled.header`
-  .header-wrapper{
+  display: flex;
+  position: relative;
+
+  min-height: 300px; width: 100%;
+  padding: 50px 0 85px 0;
+
+  .bg-image{
+    position: absolute;
+    width: 100%;height: 100%;
+
+    top: 0;
+    object-fit: cover;
+    z-index: -1;
+  }
+  .title-button-wrapper{
     display: flex;
-    position: relative;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 20px;
 
-    height: auto;width: 100%;
-    padding: 50px 0 85px 0;
-
-    .bg-image{
-      position: absolute;
-      width: 100%;height: 100%;
-  
-      top: 0;
-      object-fit: cover;
-      z-index: -1;
+    > h1{
+      color: var(--very-light-gray);
+      height: 31px;
+      letter-spacing: 10px;
     }
-    .title-button-wrapper{
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      margin-bottom: 20px;
 
-      > h1{
-        color: var(--very-light-gray);
-        height: 31px;
-        letter-spacing: 10px;
-      }
-
-      > button{
-        background-color: transparent;
-        border: none;
-        cursor: pointer;
-        height: fit-content;
-      }
-
+    > button{
+      background-color: transparent;
+      border: none;
+      cursor: pointer;
+      height: fit-content;
     }
+
   }
 `;
 
@@ -58,32 +56,30 @@ export default function Header() {
 
   return (
     <StyledHeader>
-      <header className="header-wrapper">
-        <img 
-          src={`/images/${mobileDarkModeImg}`} 
-          alt="Imagen desktop dark mode" 
-          className="bg-image"
-        />
+      <img 
+        src={`/images/${mobileDarkModeImg}`} 
+        alt="Imagen desktop dark mode" 
+        className="bg-image"
+      />
 
-        <div className="container">
-          <div className="title-button-wrapper">
+      <div className="container">
+        <div className="title-button-wrapper">
 
-            <h1>TODO</h1>
-            <button
-              // onClick={ () => ( darkMode ? setDarkMode(false) : setDarkMode(true) )}
-              // onChange={handleChange}
-            >
-              <img src={`/images/${iconSun}`} alt="Imagen para cambiar a dark/light mode" />
-            </button>
+          <h1>TODO</h1>
+          <button
+            // onClick={ () => ( darkMode ? setDarkMode(false) : setDarkMode(true) )}
+            // onChange={handleChange}
+          >
+            <img src={`/images/${iconSun}`} alt="Imagen para cambiar a dark/light mode" />
+          </button>
 
-          </div>
-
-          <div className="form-wrapper">
-            <input type="checkbox" id="create_task" />
-            <input type="text" id="text_todo" placeholder="Crear nuevo todo..." />
-          </div>
         </div>
-      </header>
+
+        <div className="form-wrapper">
+          <input type="checkbox" id="create_task" />
+          <input type="text" id="text_todo" placeholder="Crear nuevo todo..." />
+        </div>
+      </div>
     </StyledHeader>
   )
 }
